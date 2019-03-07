87f633a06c29e488147f65779cd2a24722dae970
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-03-07 12:52:09 -0500

    add nvm source lines
    this allows nvm to be recognized as a command in the shell that was
    spawned by the script

--- install_apps.sh
+++ install_apps.sh
@@ -44,6 +44,9 @@ fi
 if [ ! -x "$(command -v nvm)" ]; then
   echo "Installing Node Version Manager."
   brew install nvm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   nvm install --lts
   nvm alias default 'lts/*'
 else
